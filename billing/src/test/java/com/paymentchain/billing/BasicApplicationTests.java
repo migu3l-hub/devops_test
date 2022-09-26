74a0a8a41ea6c6a8a61ceee3a916462af54e0f93
migu3l-hub <cachecuas1@gmail.com>
2022-09-26 13:38:45 -0500

    add integratin mockito que es un framework spring

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-         String message= "default message cambio 1";
+         String message= "default message cambio 1 pero ahora es el momento de hacer las pruebas de integracion continua eliminando el merge manual de github";
          Assert.assertNotNull(message);
 	}
 
