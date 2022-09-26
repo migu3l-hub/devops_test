3216300c89f461f0287fe41bc387c1cc4401fc8a
migu3l-hub <cachecuas1@gmail.com>
2022-09-26 14:41:01 -0500

    notificaciones con slack prueba 1

--- billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
+++ billing/src/test/java/com/paymentchain/billing/BasicApplicationTests.java
@@ -11,7 +11,7 @@ public class BasicApplicationTests {
 
 	@Test
 	public void contextLoads() {
-         String message= "default message cambio 1 pero ahora es el momento de hacer las pruebas de integracion continua eliminando el merge manual de github";
+         String message= "default message cambio 1 pero ahora es el momento de hacer las pruebas de integracion continua eliminando la rama de github y ademas enviando una notificacion de slack";
          Assert.assertNotNull(message);
 	}
 
